47b790205acb5fb74ac9fd8e548ab657233c2bac
stackTom <rtiky23@gmail.com>
2015-12-28 20:13:59 -0500

    cleaned up code, used google's class to provide a working extremely basic example

--- test1/mainMap.js
+++ test1/mainMap.js
@@ -1,78 +1,5 @@
-// subclassing would have been nice here, but subclassing in JS is kind of really annoying
-// from the basics I've seen...
-function GreenCircle(mapToDisplayOn, centerPoint) {
-	this.gCircle =  new google.maps.Circle({
-      strokeColor: "green",
-      strokeOpacity: 0.8,
-      strokeWeight: 2,
-      fillColor: "green",
-      fillOpacity:1,
-      map: mapToDisplayOn,
-      center: centerPoint,
-      radius: 1000000 // big on purpose so you can see it
-    });
-
-	this.gCircle.addListener("click", function() {
-    	console.log("I WAS CLICKED");
-    });
-}
-
-// jQuery find is way too slow. No idea what sort of magic Google uses to parse KML's into
-// placemarks so quickly. EDIT: an optimization I made to make it quicker: find only "Document"
-// in the data and not each placemark, as this was way too slow
-function customKmlLayer(url, map, marker) {
-	this.placemarks = [];
-	var oldThis = this;
-	//url = "doc.kml";
-
-	$.get(url, function(data) {
-		//loop through placemarks tags
-		$(data).find("Document").each(function(index, value) {
-			//get coordinates and place name
-			var findObj = $(this).find("coordinates");
-			var coords = [];
-			$(this).find("coordinates").each(function() {
-				coords.push(this.innerHTML);
-			});
-			var place = $(this).find("name").text();
-			var numCoords = coords.length;
-			/*var img = {
-				url: "http://maps.gstatic.com/mapfiles/ridefinder-images/mm_20_green.png",
-				origin: new google.maps.Point(0, 0),
-				scaledSize: new google.maps.Size(7, 8)
-			};*/
-			for (var i = 0; i < numCoords; i++) {
-				var c = coords[i].split(",");
-
-				var marker = new google.maps.Marker({
-					position: { lat: parseFloat(c[1]), lng: parseFloat(c[0]) },
-					//icon: img,
-					//map: map,
-					title: "place"
-				});
-				marker.addListener("click", function() {
-					console.log(this.position.lat());
-				});
-				oldThis.placemarks.push(marker);
-			}
-		});
-		// jQuery GET is async, so we gotta figure out a way to call a func to create the
-		// markers on the map, else we might get an undefined array. example, when the below
-		// two lines were below, the get async thread wouldn't finish in time before these two
-		// lines were called, leading to an undefined array. solution: pass an onDone function
-		// to thi function
-		marker.addMarkers(oldThis.placemarks);
-		console.log("done marker");
-	});
-}
-
-function doSetTimeout(i, map, cluster) {
-	setTimeout(function() {
-			var ctaLayer = new customKmlLayer("https://raw.githubusercontent.com/stackTom/gmapsTestKML/master/test_kml/file" + i + ".kml",
-							map, cluster);
-			console.log("done layer " + i);
-		}, 1000);
-}
+var gmap;
+var layers = [];
 
 function mapInit() {
 	var mapOpts = {
@@ -80,39 +7,41 @@ function mapInit() {
 		zoom: 1
 	};
 
-	var gmap = new google.maps.Map(document.getElementById("map-container"), mapOpts);
-	var test = new MarkerClusterer(gmap);
+	gmap = new google.maps.Map(document.getElementById("map-container"), mapOpts);
 
-	// kml method. this kml file has 25,000 points (the limit imposed by google per kml file)
-	// i think one can use a max of 5 kml files
+	// add event listener on the map to disable the popup created by clicking on a marker
+	google.maps.event.addListener(gmap, "click", function(mapEvent) {
+		var popup = document.getElementById("popup");
+		popup.innerHTML = ""; // since the div hasn't a specified height, setting its html to an empty string will make it disappear
+	});
 
-	for (var i = 1; i < 13; i++) {
-		doSetTimeout(i, gmap, test);
+	// load in our kml's
+	for (var i = 0; i < 13; i++) {
+		var ctaLayer = new google.maps.KmlLayer({
+   			url: "https://raw.githubusercontent.com/stackTom/gmapsTestKML/master/test_kml/file" + i + ".kml",
+   			preserveViewport: true, // don't center view the KML's when we render them
+    		suppressInfoWindows: true
+  		});
+
+  		layers.push(ctaLayer);
+
+  		google.maps.event.addListener(ctaLayer, "click", function(KmlEvent) {
+  			// add the popup...
+  			var popup = document.getElementById("popup"); // this can be styled much better... just an example
+  			popup.innerHTML = "My coords are " + KmlEvent.latLng.toString();
+  		});
 	}
 
-  	/*google.maps.event.addListener(ctaLayer, "click", function(event) {
-  		console.log(event.featureData);
-  	});*/
+	// only render the markers on the map past a certain zoom level to speed things up
+	google.maps.event.addListener(gmap, "zoom_changed", function() {
+		for (var i = 0; i < 13; i++) {
+			if (gmap.getZoom() > 5) {
+				layers[i].setMap(gmap);
+			} else {
+				layers[i].setMap(null);
+			}
+		}
+	});
 
-	// my test circle/point class. one could have a loop to plot these. i am working on a test for this now
-	// so we can determine which is more efficient/practical for our purposes. might get it done today as it
-	// requires me to have a python script which extracts coordinates from a kml file so we can use those coords
-	// and not have to come up with 25k coords by ourselves...
-  	//var testCircle = new GreenCircle(gmap, gmap.getCenter());
-
-
-  	// slow as hell. maybe i have a bug? maybe so as the circles aren't coming out in the right place
-  	// asynchronouse, gonna make a json file out of the coordinates
-  	/*$.getJSON("test.json", function(json) {
-  		console.log(json);
-
-  		var arLength = json.points.length - 20000;
-
-  		for (var i = 0; i < arLength; i++) {
-  			var circle = new GreenCircle(gmap, json.points[i]);
-  		}
-  		console.log("done");
-	});*/
-
-	console.log("map loaded");
+	console.log("done loading layers");
 }
